994cc187b50ce19d926c69acc1248328adb2499b
Emir Buckun <emirbuckun@hotmail.com>
2023-06-15 21:32:52 +0300

    Program completed

--- ShoppingReceiptParser/Program.cs
+++ ShoppingReceiptParser/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+string result = "";
+
+// Read response.json file and parse its description property of the first object
+using (StreamReader r = new("response.json"))
+{
+  string json = r.ReadToEnd();
+  dynamic? array = JsonConvert.DeserializeObject(json);
+  result = array is not null ? array[0].description : "Couldn't parse the data.";
+}
+
+// Print result to txt file
+File.WriteAllText("output.txt", result);
